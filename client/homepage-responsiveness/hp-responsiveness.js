9afbba5cbdc358e71bb166bf7c4228608eea313c
David Bijl <davidbijl321@gmail.com>
2025-06-02 09:36:40 +0200

    comit voor maarten

--- client/homepage-responsiveness/hp-responsiveness.js
+++ client/homepage-responsiveness/hp-responsiveness.js
@@ -1,3 +1,5 @@
-// document.documentElement.style.setProperty("--" + rangeName, rangeValue);
+document.addEventListener('mousemove', (e) => {
 
-
+    let recalculatedX = 100 - (e.clientX / window.innerWidth) * 100;
+    document.documentElement.style.setProperty('--value', recalculatedX + '%');
+});
