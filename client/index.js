537bae74cfa5462764c5ef32e576957d1f344ff9
TomvanD298 <tom@realafford.nl>
2025-06-04 13:38:20 +0200

    added crocs

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import { log } from "./debug.js";
 console.log("Hello, world!");
 
 // Views
-import '../server/views/projects/projects.js';
+import "../server/views/projects/projects.js";
 
 // Components
 // import "../server/components/category-filter/category-filter.js";
@@ -13,6 +13,7 @@ import '../server/views/projects/projects.js';
 import "../server/components/full-width-card/full-width-card.js";
 import "../server/components/project-card/project-card.js";
 import "../server/components/text-slider/text-slider.js";
+import "../server/components/full-width-crocodile/crocodile.js";
 import "../server/components/break-glass/break-glass.js";
 import "../server/components/submarine/submarine.js";
 
