6e8955af043eb66a2dc74cfe5e13f94fd72a8734
StijnCSS <stijnzwaard@gmail.com>
2025-06-06 17:11:13 +0200

    base

--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import '../server/components/category-filter/category-filter.js';
 import '../server/components/project-sorting/project-sorting.js';
 import '../server/components/search-bar/search-bar.js';
 import '../server/components/full-width-card/full-width-card.js';
+import '../server/components/stage-lights/stage-lights.js';
 
 const _DebugBool = true;
 const _fileName = "server";
