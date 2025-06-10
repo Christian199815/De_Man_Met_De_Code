7a49505cd7162f4c7256949c54301964e6aefbae
Christian Donker <45289692+Christian199815@users.noreply.github.com>
2025-06-10 22:28:12 +0200

    fixed crocs and project card height

--- server/components/full-width-crocodile/crocodile.js
+++ server/components/full-width-crocodile/crocodile.js
@@ -21,7 +21,7 @@ function initializeCrocodiles() {
     return;
   }
 
-  const NUM_CROCS = Math.floor(Math.random() * 6) + 4;
+  const NUM_CROCS = Math.floor(Math.random() * 16) + 4;
   const CROCODILE_SRC = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/be02a4e3-e357-4bde-b048-3a8a59e11d49/d2xqipq-6a19af1e-898b-4214-880e-ddf321d86f7a.png/v1/fill/w_1173,h_681/crocodile_png_by_absurdwordpreferred_d2xqipq-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjQwNyIsInBhdGgiOiJcL2ZcL2JlMDJhNGUzLWUzNTctNGJkZS1iMDQ4LTNhOGE1OWUxMWQ0OVwvZDJ4cWlwcS02YTE5YWYxZS04OThiLTQyMTQtODgwZS1kZGYzMjFkODZmN2EucG5nIiwid2lkdGgiOiI8PTQxNDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.7J0KJsi-zeG_BtEHujYNY047XYwTSOug40ggbE4Dnqw";
 
   // Create crocodiles
@@ -44,13 +44,13 @@ function initializeCrocodiles() {
     
     // Initial direction (start facing left)
     let facingRight = false;
-    wrapper.style.transform = "scaleX(-1)";
+    wrapper.style.transform = "rotateY(180deg)";
     
     // Animation direction change
     wrapper.addEventListener("animationiteration", (e) => {
       if (e.animationName === "walk") {
         facingRight = !facingRight;
-        wrapper.style.transform = facingRight ? "scaleX(1)" : "scaleX(-1)";
+        wrapper.style.transform = facingRight ? "rotateY(0deg)" : "rotateY(180deg)";
       }
     });
     
